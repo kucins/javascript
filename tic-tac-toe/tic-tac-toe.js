fc52d4e83bbeb321452d4c9fc32ca49e0779d437
kucins <kubracinarlar@gmail.com>
2017-10-17 20:46:19 +0300

    ilk commit

---
+++ tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,48 @@
+function startGame(){
+	document.turn ="X";
+}
+function NextMove(kare){
+	if(kare.innerText==""){
+		kare.innerText=document.turn;
+		SwitchTurn();
+	}
+}
+
+function SwitchTurn(){
+
+	if(CheckWinner(document.turn))
+		document.getElementById("p1").innerText = "KAZANAN " + document.turn;
+
+	if(document.turn=="X")
+		document.turn="O";
+	else
+		document.turn="X";
+}
+
+function CheckWinner(move){
+	var result= false;
+	if(
+		CheckRow(1,2,3,move) ||
+		CheckRow(4,5,6,move) || 
+		CheckRow(7,8,9,move) || 
+		CheckRow(1,5,9,move) || 
+		CheckRow(3,5,7,move) ||
+		CheckRow(1,4,4,move) ||
+		CheckRow(2,5,8,move) ||
+		CheckRow(3,6,9,move))
+	{
+		result = true;
+	}
+		return result;
+}
+function CheckRow (x,y,z,move){
+	var result = false;
+	if(getBox(x)==move && getBox(y)==move && getBox(z) == move)
+	{
+		result= true;
+	}
+	return result; 
+}
+function getBox(number){
+		return document.getElementById("k"+ number).innerText;
+}
