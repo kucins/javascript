840b0c3c75356dfc57bf5bbe544cd8905730859b
kucins <kubracinarlar@gmail.com>
2017-10-18 11:17:44 +0300

    Polindrom kelimeleri kontrol

---
+++ palindrome.js
@@ -0,0 +1,6 @@
+function palindrome(str) {
+  var newstr = str.toLowerCase().replace(/[\W_]/g,'');
+  return newstr == str.toLowerCase().replace(/[\W_]/g,'').split('').reverse().join('');
+}
+
+palindrome("eye");
